f1b772b6549d3ab3e06b57d4702a3576e33bed23
LeeMinglu <402007353@qq.com>
2016-08-29 22:02:11 +0800

    初始化环境

---
+++ swift_group-purchase/swift_group-purchase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift_group-purchase
+//
+//  Created by 李明禄 on 16/8/29.
+//  Copyright © 2016年 SocererGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
