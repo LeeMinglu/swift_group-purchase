0fba0cf67d8927fd2e235bc150ecf55bbc999703
LeeMinglu <402007353@qq.com>
2016-08-30 10:12:17 +0800

    使用系统自带的cell显示

--- swift_group-purchase/swift_group-purchase/Base.lproj/Controller/ViewController.swift
+++ swift_group-purchase/swift_group-purchase/Base.lproj/Controller/ViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableView: UITableView!
+    let ScreenRect = UIScreen.mainScreen().bounds
+    
     let pathString = "tgs.plist"
     var GoodsModelArray: [LSGoods] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //初始化数据
+        loadData()
         
-        initData()
+        //添加tableView
+        addTableView()
+        
+        
     }
     
-    func initData() {
+    // MARK:   加载数据
+    func loadData() {
         let path = NSBundle.mainBundle().pathForResource(pathString, ofType: nil)
         let GoodsArray = NSArray(contentsOfFile: path!)!
         
@@ -34,6 +44,58 @@ class ViewController: UIViewController {
         print(self.GoodsModelArray)
         
     }
+    
+    // MARK: 添加tableView
+    func addTableView() {
+        let tableViewX = 0 as CGFloat
+        let tableViewY = 0 as CGFloat
+        let tableViewWidth = self.ScreenRect.width
+        let tableViewHeight = self.ScreenRect.height
+        let frame = CGRectMake(tableViewX, tableViewY, tableViewWidth, tableViewHeight)
+        let tempTableView = UITableView.init(frame: frame, style: UITableViewStyle.Plain)
+        
+        self.tableView = tempTableView
+        self.view.addSubview(self.tableView)
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.contentInset = UIEdgeInsetsMake(60, 0, 0, 0)
+    
+    }
+    
+    
+    // MARK:  DataSource Method
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.GoodsModelArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let ID = "good"
+        var cell = tableView.dequeueReusableCellWithIdentifier(ID)
+        
+        if cell == nil {
+           cell = UITableViewCell.init(style: UITableViewCellStyle.Subtitle, reuseIdentifier: ID)
+        }
+        
+        let good = self.GoodsModelArray[indexPath.row]
+        cell?.textLabel?.text = good.title
+        cell?.imageView?.image = UIImage(named: good.icon)
+        cell?.detailTextLabel?.text = good.price
+        
+        return cell!
+    }
+    
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 80
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
